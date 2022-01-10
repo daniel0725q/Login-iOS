45ae23d2734c1fccee928dfe3bb174ef79e66c2d
Daniel Quintero <daniel0725q@gmail.com>
2022-01-09 19:12:22 -0500

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Daniel on 9/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
