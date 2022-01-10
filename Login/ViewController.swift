c3a7cd496c937aacdb4817fbe3984fbb5a3bad5c
Daniel Quintero <daniel0725q@gmail.com>
2022-01-09 19:47:51 -0500

    Added two buttons, and sent programmatically the title for the next segue, override prepare for segue function.

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "nextView", sender: sender)
+    }
+    
+    @IBAction func forgotUsernameButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "nextView", sender: sender)
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "nextView", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else { return }
+        
+        if sender == forgotUsernameButton {
+            segue.destination.title = "Forgot Username"
+        } else if sender == forgotPasswordButton {
+            segue.destination.title = "Forgot Password"
+        } else {
+            segue.destination.title = usernameTextField.text
+        }
+    }
 }
 
